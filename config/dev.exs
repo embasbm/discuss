62cb631eb8d80e5a968f5cef674e83dc5d9ead94
Emba Moussa <embasbm@gmail.com>
2020-01-21 23:49:16 +0100

    move stuff to secret file

--- config/dev.exs
+++ config/dev.exs
@@ -33,11 +33,3 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
-config :discuss, Discuss.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "emba",
-  password: "",
-  database: "discuss_dev",
-  hostname: "localhost",
-  pool_size: 10
